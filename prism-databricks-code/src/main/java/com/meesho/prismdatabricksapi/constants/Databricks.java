2c1bf69b8d417dd09cbb1bf6479969c54e1fb739
NishchayAgarwal <106945597+NishchayAgarwal@users.noreply.github.com>
2022-08-04 23:56:56 +0530

    change

--- prism-databricks-code/src/main/java/com/meesho/prismdatabricksapi/constants/Databricks.java
+++ prism-databricks-code/src/main/java/com/meesho/prismdatabricksapi/constants/Databricks.java
@@ -1,11 +1,6 @@
 package com.meesho.prismdatabricksapi.constants;
 
 public class Databricks {
-//    public static String URL = "https://metabase-temp.meesho.com";
-    public static String URL = "https://metabase-presto.meesho.com";
-    public static String CARD_ID = "CARD_ID";
     public static String DASHBOARD_ID = "DASHBOARD_ID";
-    public static String EXECUTE_QUESTION_ENDPOINT = "/api/card/" + CARD_ID + "/query";
-    public static String CREATE_SESSION_ENDPOINT = "/api/session";
-    public static String EXECUTE_DASHBOARD_ENDPOINT = "/api/dashboar/" + DASHBOARD_ID + "/query";
+
 }
